8148a219eb1a5372bb6548d7128dd6985d3a3e76
sparshy66-eng <sparshy66@gmail.com>
2026-07-15 16:00:33 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBYPok8vVFZCDpmgZywxJrKtAhy13wMGLI",
-  authDomain: "cindertune-cad8b.firebaseapp.com",
-  databaseURL: "https://cindertune-cad8b-default-rtdb.firebaseio.com",
-  projectId: "cindertune-cad8b",
-  storageBucket: "cindertune-cad8b.firebasestorage.app",
-  messagingSenderId: "865518101222",
-  appId: "1:865518101222:web:44d9cee2be58f603d51127"
-};
